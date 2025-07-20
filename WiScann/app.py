0320dc14af7c96be95510bb51d6a60a169b640fb
LTX74 <aaronpernet@gmail.com>
2025-07-20 03:33:01 +0200

    Update app.py

--- WiScann/app.py
+++ WiScann/app.py
@@ -128,7 +128,7 @@ def index():
 
 @app.route("/scan")
 def scan():
-    network = "192.168.1.0/24"
+    network = "192.x.x.x/24"     #^^^^^^^^^^Changement ICI !^^^^^^^^^^
     devices = scan_network(network)
     return jsonify(devices)
 
